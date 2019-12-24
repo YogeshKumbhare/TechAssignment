82cf2f15bb788b26f1747f86001be12d206a5e98
yogesh_kumbhare <yogeshkumbhare2012@gmail.com>
2019-12-24 22:52:58 +0530

    commit
    Change-Id: I2d828b5095341496394f559c517d20d2fda5216d
    Signed-off-by: yogesh_kumbhare <yogesh_kumbhare@BLL07252.persistent.co.in>

--- TechAssign/src/main/java/com/hashtaag/TechAssign/Controller/SearchController.java
+++ TechAssign/src/main/java/com/hashtaag/TechAssign/Controller/SearchController.java
@@ -1,13 +1,11 @@
 package com.hashtaag.TechAssign.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hashtaag.TechAssign.Model.DistrictDetails;
@@ -21,8 +19,8 @@ public class SearchController {
 	@Autowired
 	private SearchService searchService;
 
-	@RequestMapping(value = "/state/{state}", method = RequestMethod.GET)
-	public List<State> getStateDetails(@PathVariable String state) {
+	@RequestMapping(value = "/state", method = RequestMethod.GET)
+	public List<State> getStateDetails(@RequestParam(value = "q") String state) {
 
 		
 		List<State> searchStateResults = null;
@@ -31,8 +29,8 @@ public class SearchController {
 
 	}
 
-	@RequestMapping(value = "/town/{town}", method = RequestMethod.GET)
-	public List<TownDetails> getTownDetails(@PathVariable String town) {
+	@RequestMapping(value = "/town", method = RequestMethod.GET)
+	public List<TownDetails> getTownDetails(@RequestParam(value = "q") String town) {
 
 		List<TownDetails> searchTownDetails = null;
 		searchTownDetails  = searchService.getAllTownDetails(town);
@@ -40,8 +38,8 @@ public class SearchController {
 
 	}
 	
-	@RequestMapping(value = "/district/{district}", method = RequestMethod.GET)
-	public List<DistrictDetails> getDistrictDetails(@PathVariable String district) {
+	@RequestMapping(value = "/district", method = RequestMethod.GET)
+	public List<DistrictDetails> getDistrictDetails(@RequestParam(value = "q") String district) {
 
 		List<DistrictDetails> dtDetails = null;
 		dtDetails  = searchService.getAllDistrictDetails(district);
